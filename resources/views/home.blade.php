521e50edb10c02147c3fae617546377d3f9a2f4e
ayu2000y15 <ayu2000y15@nasystem.net>
2025-03-23 12:35:55 +0900

    　　日付：2025/03/23 　変更者：野口 変更内容：文字の配置微調整

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -62,71 +62,74 @@
             <div class="text company">
                 <table class="table company">
                     @foreach ($textCompany as $company)
-                        <tr>
-                            <th>
-                                {{$company["view_name"]}}
-                            </th>
-                            <td style="color: #ac2737;">
-                                @if ($company["type"] == "date")
-                                    {{ date('Y年n月j日', strtotime($company["value"])) }}
-                                @else
-                                    {!! nl2br($company["value"]) !!}
-                                @endif
-                            </td>
+                                <tr>
+                                    <th>
+                                        {{$company["view_name"]}}
+                                    </th>
+                                    <td>
+                                        @if ($company["type"] == "date")
+                                            {{ date('Y年n月j日', strtotime($company["value"])) }}
+                                        @elseif($company["type"] == "textarea")
+                                            {!! nl2br($company["value"]) !!}
+                                        @else
+                                            {{ $company["value"] }}
+                                        @endif
+                                    </td>
+                        </div>
                         </tr>
                     @endforeach
-                </table>
-            </div>
+            </table>
         </div>
+    </div>
 
-        <div class="content philosophy">
-            <div class="title philosophy">
-                @if($titlePhilosophy == null)
-                    <div class="title-content">
-                        <img src="{{ asset($logoImg1->file_path . $logoImg1->file_name) }}" alt="ロゴ">
-                        <h1>Philosophy</h1>
-                    </div>
-                @else
-                    <img src="{{ asset($titlePhilosophye->file_path . $titlePhilosophy->file_name) }}" alt="タイトル画像">
-                @endif
-            </div>
-            <hr class="line">
-            <div class="text philosophy">
-                <p>{!! nl2br($textPhilosophy->content) !!}</p>
-            </div>
+    <div class="content philosophy">
+        <div class="title philosophy">
+            @if($titlePhilosophy == null)
+                <div class="title-content">
+                    <img src="{{ asset($logoImg1->file_path . $logoImg1->file_name) }}" alt="ロゴ">
+                    <h1>Philosophy</h1>
+                </div>
+            @else
+                <img src="{{ asset($titlePhilosophye->file_path . $titlePhilosophy->file_name) }}" alt="タイトル画像">
+            @endif
         </div>
-
-        <div class="content contact">
-            <div class="title contact">
-                @if($titleContact == null)
-                    <div class="title-content">
-                        <img src="{{ asset($logoImg1->file_path . $logoImg1->file_name) }}" alt="ロゴ">
-                        <h1>Contact</h1>
-                    </div>
-                @else
-                    <img src="{{ asset($titleContact->file_path . $titleContact->file_name) }}" alt="TOP画像">
-                @endif
-                <span class="business-hours">※平日12:00~19:00</span>
-            </div>
-            <hr class="line">
-            <div class="text contact">
-                <table class="table contact">
-                    @foreach ($textContact as $contact)
-                        <tr>
-                            <th>
-                                {{$contact["view_name"]}}
-                            </th>
-                            <td style="color: #ac2737;">
-                                {!! nl2br($contact["value"]) !!}
-                            </td>
-                        </tr>
-                    @endforeach
-                </table>
-            </div>
+        <hr class="line">
+        <div class="text philosophy">
+            <p>{!! nl2br($textPhilosophy->content) !!}</p>
         </div>
+    </div>
 
-        <footer class="footer">
-            <img src="{{ asset($logoImg2->file_path . $logoImg2->file_name) }}" alt="ロゴ">
-        </footer>
+    <div class="content contact">
+        <div class="title contact">
+            @if($titleContact == null)
+                <div class="title-content">
+                    <img src="{{ asset($logoImg1->file_path . $logoImg1->file_name) }}" alt="ロゴ">
+                    <h1>Contact</h1>
+                </div>
+            @else
+                <img src="{{ asset($titleContact->file_path . $titleContact->file_name) }}" alt="TOP画像">
+            @endif
+            <span class="business-hours">※平日12:00~19:00</span>
+        </div>
+        <hr class="line">
+        <div class="text contact">
+            <table class="table contact">
+                @foreach ($textContact as $contact)
+                    <tr>
+                        <th>
+                            {{$contact["view_name"]}}
+                        </th>
+                        <td style="color: #ac2737;">
+                            {!! nl2br($contact["value"]) !!}
+                        </td>
+                    </tr>
+                @endforeach
+            </table>
+        </div>
+    </div>
+
+    <footer class="footer">
+        <img src="{{ asset($logoImg2->file_path . $logoImg2->file_name) }}" alt="ロゴ">
+    </footer>
     </div>
 @endsection
